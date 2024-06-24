be5081d57061eca4804c2ce63f2455d0f5a3ddaa
Elmer Zapata <elmer.765@gmail.com>
2024-06-24 19:23:44 -0400

    add Pokemon test API

---
+++ src/gatling/java/pokemonapi/PokemonApiTest.java
@@ -0,0 +1,52 @@
+package pokemonapi;
+
+
+
+import java.time.Duration;
+import java.util.*;
+
+import io.gatling.javaapi.core.*;
+import io.gatling.javaapi.http.*;
+
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.*;
+
+public class PokemonApiTest extends Simulation {
+    // Case 1 - Como puedo testearlo en diferentes ambientes
+    String baseUrl = System.getProperty("baseUrl", "https://pokeapi.co/api/v2/pokemon");
+    // Define the data
+    FeederBuilder.FileBased<Object> feeder = jsonFile("data/pokemon.json").circular();
+    // Define preconditions
+    // We want to test the Pokemon API with 5 pokemons
+    //
+    // Define the base URL and headers
+    private HttpProtocolBuilder httpProtocol = http
+            .baseUrl(baseUrl);
+
+
+    // Define the scenario
+
+
+    ScenarioBuilder scn = scenario("Pokemon API Test")
+            .feed(feeder)
+            .exec(http("Get Pikachu")
+                    .get("/#{pokemonName}")
+                    .check(jmesPath("base_experience").isEL("#{baseExperience}"))
+                    .check(status().is(200)))
+            ;
+    // Case 2 adicionar assert en el body
+    // Set up the scenario
+
+    {
+        setUp(
+                scn.injectOpen(
+                        rampUsers(10).during(10)
+                )
+        ).protocols(httpProtocol);
+    }
+
+
+
+
+
+}
